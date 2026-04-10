7ed8f25153b0ebcbbcd0a0e9f9ae840e7abb44f0
Antonio Roldán <antonioroldan95@outlook.com>
2026-04-10 19:59:16 +0200

    Implement main class for permutations solution
    Added main class to find permutations with unique characters.

---
+++ Main.java
@@ -0,0 +1,14 @@
+package CompetitiveProgrammingSolutions;
+
+//PROBLEM STATEMENT: FIND ALL PERMUTATIONS OF S AND RETURN LIST OF PERMUTATIONS WITH UNIQUE CHARACTERS AND PRINT LIST OF UNIQUE CHARACTERS EACH PERMUTATION HAS
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        PermutationsWithUniqueCharactersSolution solution = new PermutationsWithUniqueCharactersSolution();
+        System.out.println(solution.lengthOfLongestSubstring("qwwwwerrrtyy"));
+
+    }
+}
+
